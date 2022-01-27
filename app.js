1bb1d558bed8ec0418a6a8141cf977598a72ffe6
konivan <69760099+konivan@users.noreply.github.com>
2022-01-27 12:20:24 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -18,6 +18,19 @@ let food = {
 };
 
 
+function change_color() {
+    let hex_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "A", "B", "C", "D", "E", "F"];
+
+    let hexcode = '';
+    for(let i = 0; i < 6; i++) {
+        let random_index = Math.floor(Math.random() * hex_numbers.length);
+
+        hexcode += hex_numbers[random_index];
+    };
+    document.getElementsByTagName('body')[0].style.background = "#" + hexcode;
+};
+
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
